02459801d29b3e379b19d72eae31e0cc65c98799
Kishore <Kishore@DESKTOP-GDFIP89>
2019-12-16 17:20:02 +0530

    1

---
+++ src/qsp/Launch.java
@@ -0,0 +1,22 @@
+package qsp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Launch {
+	
+	@Test
+	public void test1() throws InterruptedException
+	{
+		System.setProperty("webdriver.gecko.driver","./drivers/geckodriver.exe");
+		
+		WebDriver driver =new FirefoxDriver();
+		
+		driver.get("https://www.google.com");
+		Thread.sleep(5000);
+		
+		driver.quit();
+	}
+
+}
